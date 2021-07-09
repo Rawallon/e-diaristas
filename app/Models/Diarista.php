baba457db1c2190969b89b3be57ed1c03e1ebc12
rawallon <rawallon@gmail.com>
2021-07-09 16:45:15 -0300

    Aula 2

--- app/Models/Diarista.php
+++ app/Models/Diarista.php
@@ -8,5 +8,89 @@ use Illuminate\Database\Eloquent\Model;
 class Diarista extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome_completo', 'cpf', 'email', 'telefone', 'logradouro', 'numero', 'complemento', 'bairro', 'cidade', 'estado', 'cep', 'foto_usuario'];
+
+    /**
+     * Define os campos que podem ser gravados
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome_completo',
+        'cpf',
+        'email',
+        'telefone',
+        'logradouro',
+        'numero',
+        'complemento',
+        'bairro',
+        'cidade',
+        'estado',
+        'cep',
+        'codigo_ibge',
+        'foto_usuario'
+    ];
+
+    /**
+     * Define os campos a serem retornados
+     *
+     * @var array
+     */
+    protected $visible = [
+        'nome_completo',
+        'cidade',
+        'foto_usuario',
+        'reputacao'
+    ];
+
+    /**
+     * Adiciona campos no retorno
+     *
+     * @var array
+     */
+    protected $appends = ['reputacao'];
+
+    /**
+     * Monta URL na imagem
+     *
+     * @param string $valor
+     * @return void
+     */
+    public function getFotoUsuarioAttribute($valor)
+    {
+        return config('app.url') . '/' . $valor;
+    }
+
+    /**
+     * Retorna um valor aleatorio a reputacao
+     *
+     * @param string $valor
+     * @return void
+     */
+    public function getReputacaoAttribute($valor)
+    {
+        return mt_rand(1, 5);
+    }
+
+    /**
+     * Busca diaristas por código do IBGE
+     *
+     * @param integer $codigoIbge
+     * @return void
+     */
+    static public function buscaPorCodigoIbge(int $codigoIbge)
+    {
+        return self::where('codigo_ibge', $codigoIbge)->limit(6)->get();
+    }
+
+    /**
+     * Lista o numero restante de diaristas a ser exibido
+     *
+     * @param integer $codigoIbge
+     * @return void
+     */
+    static public function quantidadePorCodigoIbge(int $codigoIbge)
+    {
+        $quantidade = self::where('codigo_ibge', $codigoIbge)->limit(6)->count();
+        return $quantidade > 6 ? $quantidade - 6 : 0;
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,9 @@
 @section('titulo', 'Página Inicial')
 
 @section('conteudo')
+
 <h1>Lista de Diaristas</h1>
+
 <table class="table">
   <thead>
     <tr>
@@ -15,24 +17,29 @@
   </thead>
   <tbody>
     @forelse ($diaristas as $diarista)
+
     <tr>
       <th scope="row">{{ $diarista->id }}</th>
       <td>{{ $diarista->nome_completo }}</td>
-      <td>{{ $diarista->telefone }}</td>
+      <td>{{ \Clemdesign\PhpMask\Mask::apply($diarista->telefone, '(00) 00000-0000') }}</td>
       <td>
         <a href="{{ route('diaristas.edit', $diarista) }}" class="btn btn-primary">Atualizar</a>
         <a href="{{ route('diaristas.destroy', $diarista) }}" class="btn btn-danger" onclick="return confirm('Tem certeza que deseja apagar?')">Apagar</a>
       </td>
     </tr>
+
     @empty
+
     <tr>
       <th></th>
       <td>Nenhum registro cadastrado</td>
       <td></td>
       <td></td>
     </tr>
+
     @endforelse
   </tbody>
 </table>
+
 <a href="{{ route('diaristas.create') }}" class="btn btn-success">Nova Diarista</a>
 @endsection
