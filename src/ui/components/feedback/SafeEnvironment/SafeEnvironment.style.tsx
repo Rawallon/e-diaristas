14f1a04ce793157d963f56cb2e4b5a7a6c354573
rawallon <rawallon@gmail.com>
2021-07-09 12:33:31 -0300

    Created Laravel application

--- src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
+++
@@ -1,9 +0,0 @@
-import { experimentalStyled as styled } from '@material-ui/core/styles';
-
-export const SafeEnvorimentContainer = styled('div')`
-  color: ${({ theme }) => theme.palette.text.secondary};
-  background: ${({ theme }) => theme.palette.background.default};
-  text-align: right;
-  padding: ${({ theme }) => theme.spacing(2)} 0;
-  font-size: 12px;
-`;
--- src/ui/components/inputs/TextField/TextField.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { TextFieldStyled } from './TextField.style';
-export const TextField = TextFieldStyled;
